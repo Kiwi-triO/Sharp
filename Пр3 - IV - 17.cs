234391597ae4b280ccb5003b1c5660f76108cb6d
Kiwi-triO <58111633+Kiwi-triO@users.noreply.github.com>
2023-09-19 01:33:13 +0400

    Add files via upload

---
+++ Пр3 - IV - 17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        int k = 101; //самое первое подходящее число
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = k; j < (k + 100); j += 10) //прохожу по всем десяткам трехзначного числа
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+            k += 101;
+        }
+
+    }
+}
+
---
+++ Пр4 - VI - 19.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopwatchExample
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int count_b = (Math.Floor(Math.Sqrt(b) * Math.Sqrt(b)) == b) ? 1 : 0;
+            int count_a = (Math.Floor(Math.Sqrt(a) * Math.Sqrt(a)) == a) ? 1 : 0;
+            for (int i = 1; i * i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    count_a += 2;
+                }
+            }
+            for (int i = 1; i * i < b; i++)
+            {
+                if (b % i == 0)
+                {
+                    count_b += 2;
+                }
+            }
+            int answer = (count_a > count_b) ? a : b;
+            Console.Write("Больше делителей у числа ");
+            Console.WriteLine((answer == a) ? "a = " + a : "b = " + b);
+        }
+    }
+}
+
