f2463b7ec216e0e4a0ad60e3b0f4fafdac2f3a63
Kiwi-triO <58111633+Kiwi-triO@users.noreply.github.com>
2023-09-19 01:34:43 +0400

    Add files via upload

---
+++ Пр1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Номинал купюры = ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Количество купюр = ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("Сумма денег = " + "{0,0:C}", a * b);
+    }
+}
---
+++ Пр2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.Write("a = ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        int b = int.Parse(Console.ReadLine());
+        Console.Write("c = ");
+        int c = int.Parse(Console.ReadLine());
+        string e = ((a + b > c) && (a + c > b) && (b + c > a)) ? "YES" : "NO";
+        Console.Write($"{e}");
+
+    }
+}
