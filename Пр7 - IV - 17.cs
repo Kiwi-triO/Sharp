86c0f5bf1952978f3724be50fb5459bc4a4f49cd
Kiwi-triO <58111633+Kiwi-triO@users.noreply.github.com>
2023-09-29 10:59:47 +0400

    Add files via upload

---
+++ Пр7 - IV - 17.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+
+//class Program {
+//    static void Main(string[] args) {
+//        // Ввод размерности массива
+//        Console.Write("Введите размерность массива: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+
+//        // Создание и заполнение массива
+//        int[,] arr = new int[n, n];
+//        Console.WriteLine("Введите элементы массива:");
+//        for (int i = 0; i < n; i++)
+//        {
+//            for (int j = 0; j < n; j++)
+//            {
+//                Console.Write("a[{0},{1}]= ", i, j);
+//                arr[i, j] = Convert.ToInt32(Console.ReadLine());
+//            }
+//        }
+
+//        // Создание и заполнение массива с суммами строк
+//        int[] rowSums = new int[n];
+//        for (int i = 0; i < n; i++)
+//        {
+//            int sum = 0;
+//            for (int j = 0; j < n; j++)
+//            {
+//                sum += arr[i, j];
+//            }
+//            rowSums[i] = sum;
+//        }
+
+//        // Нахождение максимального элемента нового массива
+//        int maxElement = rowSums[0];
+//        for (int i = 1; i < n; i++)
+//        {
+//            if (rowSums[i] > maxElement)
+//            {
+//                maxElement = rowSums[i];
+//            }
+//        }
+
+//        // Вывод результата
+//        Console.WriteLine($"Максимальный элемент нового массива: {maxElement}");
+//    }
+//}
---
+++ Пр7 - VI - 7.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+
+//class Program {
+//    static void Main(string[] args) {
+//        // Ввод размерности массива
+//        Console.Write("Введите размерность массива: ");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//        int m = n;
+//        // Создание и заполнение массива
+//        int[,] arr = new int[n, n];
+//        Console.WriteLine("Введите элементы массива:");
+//        for (int i = 0; i < n; i++)
+//        {
+//            for (int j = 0; j < n; j++)
+//            {
+//                Console.Write("a[{0},{1}]= ", i, j);
+//                arr[i, j] = Convert.ToInt32(Console.ReadLine());
+//            }
+//        }
+
+//        // Проверка и удаление столбцов
+//        for (int j = 0; j < n; j++)
+//        {
+//            if (arr[0, j] > arr[n-1, j])
+//            {
+//                // Сдвиг элементов влево
+//                for (int i = 0; i < n; i++)
+//                {
+//                    for (int k = j; k < m - 1; k++)
+//                    {
+//                        arr[i, k] = arr[i, k + 1];
+//                    }
+//                }
+//                m--; // Уменьшение размерности столбцов
+//                j--; // Проверяем повторно текущий столбец, так как все столбцы сдвинулись
+//            }
+//        }
+
+//        // Вывод обработанного массива
+//        Console.WriteLine("Результат:");
+//        for (int i = 0; i < n; i++)
+//        {
+//            for (int j = 0; j < m; j++)
+//            {
+//                Console.Write(arr[i, j] + " ");
+//            }
+//            Console.WriteLine();
+//        }
+//    }
+//}
